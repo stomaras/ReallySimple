2ee8a8726173bb5346f173267583799c1b60091e
stomaras <sptoma724@gmail.com>
2020-05-17 14:08:42 +0300

    Flexible Lists

--- src/App.js
+++ src/App.js
@@ -26,14 +26,22 @@ class App extends Component {
     } )
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Tom', age: 22 },
-        { name: event.target.value, age: 29 },
-        { name: 'Maria', age: 10 }
-    ]
-    } )
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id = id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+
+    this.setState( { persons: persons } );
   }
 
   deletePersonHandler = (personIndex) => {
@@ -71,7 +79,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name = {person.name}
               age = {person.age}
-              key = {person.id} />
+              key = {person.id}
+              changed = {(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       );
