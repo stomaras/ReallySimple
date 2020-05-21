9b18d5f00ed8eab2a53622ff110a133251b6c7de
stomaras <sptoma724@gmail.com>
2020-05-21 17:24:03 +0300

    Using useEffect() in Functional Components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React , {useEffect} from 'react'
+
 import classes from './Cockpit.css'
 
 const cockpit = (props) => {
+
+    
+    useEffect(() => {
+      console.log('[Cockpit.js] useEffect');
+      // Http request ...
+    });
 
     const assignedClasses = [];
     let btnClass = '';
@@ -18,7 +25,7 @@ const cockpit = (props) => {
 
     return (
         <div className={classes.Cockpit}>
-            <h1>Hi, I'm a React App</h1>
+            <h1>{props.title}</h1>
           <p className={assignedClasses.join(' ')}>This is really working!</p>
           <button 
             className={btnClass}
