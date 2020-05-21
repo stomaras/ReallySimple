c8b67af22e10e345f9807c3d6efe6290d3ecfd1b
stomaras <sptoma724@gmail.com>
2020-05-21 16:50:12 +0300

    Component Update Lifecycle(for props Changes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Person.css';
 
 
-const person = (props) => {
-   
-    return (
+class Person extends Component { 
+    render() {
+        console.log('[Person.js] rendering...');
+        return (
         <div className={classes.Person}>
-            <p onClick={props.click}>I am a {props.name} and i am {props.age} years old</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
+            <p onClick={this.props.click}>I am a {this.props.name} and i am {this.props.age} years old</p>
+            <p>{this.props.children}</p>
+            <input type="text" onChange={this.props.changed} value={this.props.name}/>
         </div>
-    )
-};
+        )
+    }
+}
+    
 
-export default person;
+
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Person from './Person/Person'
 
-const persons = (props) => props.persons.map((person, index) => {
-        return <Person
-          click={() => props.clicked(index)}
-          name = {person.name}
-          age = {person.age}
-          key = {person.id}
-          changed = {(event) => props.changed(event, person.id)} />
-      });
+class Persons extends Component {
+        static getDerivedStateFromProps(props, state) {
+          console.log('[Persons.js] getDerivedStateFromProps');
+          return state;
+        }
 
-export default persons;
+        //gets the next props, so the upcoming props which will have an effect right after this update and the upcoming state as arguments
+        // We return true if react continue updating or false if it shouldn't 
+        shouldComponentUpdate(nextProps, nextState) {
+          console.log('[Persons.js] shouldComponentUpdate');
+          return true;
+        }
+
+        // Snapshot is a data package, so to say which you then receive in componentDidUpdate, 
+        // so that you can have some state right before the update, like the scroll position i mentioned and then you can 
+        // use it to update the DOM again or update the scrolling of the user's let's say in componentDidUpdate once the DOM 
+        // has been re-rendered and to demonstrate  this here, i will just return an object with a message of snapshot.
+        getSnapshotBeforeUpdate(prevProps, prevState){
+          console.log('[persons.js] getSnapshotBeforeUpdate');
+          return { message: 'Snapshot' };
+        }
+
+        // Will run once we are done with all the updating.
+        componentDidUpdate(prevProps, prevState, snapshot) {
+          console.log('[Persons.js] componentDidUpdate');
+          console.log(snapshot);
+        }
+
+
+        render() {
+          console.log('[Persons.js] rendering...');
+          return this.props.persons.map((person, index) => {
+            return (
+            <Person
+              click={() => this.props.clicked(index)}
+              name = {person.name}
+              age = {person.age}
+              key = {person.id}
+              changed = {(event) => this.props.changed(event, person.id)} 
+              />
+            );
+          });
+        }
+    }
+export default Persons;
